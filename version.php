fe3b8150d22448c5ae351fc7942a4c6d55ff1e0a
Paulo <pauloafpjunior@hotmail.com>
2019-08-06 21:42:07 -0300

    update version

--- version.php
+++ version.php
@@ -22,6 +22,8 @@
  */
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2019080507;         // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2019080508;         // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2015051100;         // Requires this Moodle version (see https://docs.moodle.org/dev/Releases)
+$plugin->maturity  = 'stable';         // Requires this Moodle version (see https://docs.moodle.org/dev/Releases)
+$plugin->release  = 'v1.0';         // Requires this Moodle version (see https://docs.moodle.org/dev/Releases)
 $plugin->component = 'report_usefulqueries';  // Full name of the plugin
